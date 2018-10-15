3365874d9190311af73cf1b0c5c782d62f2deba1
dineshreddy9 <tdineshreddy9@gmail.com>
2018-10-15 00:49:49 -0500

    initial commit

---
+++ src/dxt161330/RobinHood.java
@@ -0,0 +1,24 @@
+package dxt161330;
+
+public class RobinHood<T> {
+	
+	public void add(T x) {
+		
+	}
+	
+	public boolean contains(T x) {
+		return false;
+	}
+	
+	public T remove(T x) {
+		return null;
+	}
+	
+	static<T> int distinctElements(T[ ] arr) { 
+		return 0;
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
