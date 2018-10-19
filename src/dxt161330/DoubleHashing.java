be563a9a17afa4b1493426fd9b53125290555636
dineshreddy9 <tdineshreddy9@gmail.com>
2018-10-18 19:50:23 -0500

    added double hashing, modified robinhood hashing

--- src/dxt161330/DoubleHashing.java
+++ src/dxt161330/DoubleHashing.java
@@ -3,56 +3,33 @@ package dxt161330;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class RobinHood<T> implements HashingAlgorithm<T>{
-
+public class DoubleHashing<T> implements HashingAlgorithm<T> {
+	
 	Object table[];
 	// array to represent whether a particular index is free(0), an element exits(1), deleted(2) 
 	private int free[];
 	private int capacity, size;
-
-	RobinHood(int capacity){
+	
+	DoubleHashing(int capacity){
 		this.capacity = capacity;
 		table = new Object[this.capacity];
 		free = new int[this.capacity];
 		size = 0;
 	}
-
-	@Override
+	
 	public boolean add(T x) {
 
-		if(contains(x)) {
+		int location = find(x);
+		System.out.println(location);
+		if(location == -1 || contains(x)) {
 			return false;
 		}
-
-		int location = hashCode(x);
-		int displacement = 0;
-
-		while(size != capacity) {
-			if(free[location]==0 || free[location]==2) {
-				table[location] = x;
-				size++;
-				free[location] = 1;
-				return true;
-			} else if(displacement((T) table[location], location) >= displacement(x, location)) {
-				displacement++;
-				location = (location + 1) % capacity;
-			} else {
-				T temp = x;
-				x = (T) table[location];
-				table[location] = temp;
-				location = (location + 1) % capacity;
-				displacement = displacement(x, location);
-			}
-		}
-		return false;
-	}
-
-	public int displacement(T x, int loc) {
-		int i0 = hashCode(x);
-		if(loc >= i0) {
-			return (loc - i0);
-		} else {
-			return (table.length + loc - i0);
+		
+		else {
+			table[location] = x;
+			free[location] = 1;
+			size++;
+			return true;
 		}
 	}
 
@@ -60,9 +37,12 @@ public class RobinHood<T> implements HashingAlgorithm<T>{
 	public int hashCode(T x) {
 		return x.hashCode() % 19;
 	}
+	
+	public int secondHashFunction(T x) {
+		return x.hashCode();
+	}
 
 	// Checks whether the table contains x or not
-	@Override
 	public boolean contains(T x) {
 
 		int location = find(x);
@@ -74,23 +54,26 @@ public class RobinHood<T> implements HashingAlgorithm<T>{
 
 	public int find(T x) {
 
-		int ik = hashCode(x);
+		int k = 0, ik = hashCode(x);
 		while(true) {
-			
+			//ik = hashCode(x);
 			if(free[ik] == 0 || table[ik].equals(x)) {
 				return ik;
-			} else if(free[ik] == 1) {
-				break;
 			} else if(free[ik] == 2) {
-				ik = (ik + 1) % capacity;;
+				break;
+			} else if(free[ik] == 1) {
+				k++;
+				ik = (hashCode(x) + k * secondHashFunction(x)) % capacity;
+			} else if(size == capacity) {
+				return -1;
 			}
 		}
 
 		int deletedSpot = ik;
 		
 		while(true) {
-			
-			ik = (ik + 1) % capacity;
+			k++;
+			ik = (hashCode(x) + k * secondHashFunction(x)) % capacity;
 			if(!(table[ik] == null) && table[ik].equals(x)) {
 				return ik;
 			}
@@ -101,7 +84,6 @@ public class RobinHood<T> implements HashingAlgorithm<T>{
 
 	}
 
-	@Override
 	public T remove(T x) {
 		int location = find(x);
 		if(table[location].equals(x)) {
@@ -117,6 +99,4 @@ public class RobinHood<T> implements HashingAlgorithm<T>{
 	static<T> int distinctElements(T[ ] arr) { 
 		return 0;
 	}
-	
-	
 }
